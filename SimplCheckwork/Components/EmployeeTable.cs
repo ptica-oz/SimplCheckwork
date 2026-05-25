b64903ba805e9931fcf21a5b79d4d7c16a7603c4
Ирина Грибанова <gribanova@seaproject.ru>
2026-05-25 17:22:31 +0300

    Реализация асинхронной загрузки компанента

--- SimplCheckwork/Components/EmployeeTable.cs
+++ SimplCheckwork/Components/EmployeeTable.cs
@@ -1,15 +1,11 @@
 ﻿namespace SimplCheckwork.Components
 {
-    using System.Text;
-    using Checkwork.BusinessComponents.BusinessComponents;
-    using Checkwork.BusinessComponents.Helpers;
-    using Checkwork.DataAccess.Dto;
     using Microsoft.AspNetCore.Mvc;
     using SimplCheckwork.Models.Employees;
 
     public class EmployeeTable : ViewComponent
     {
-        public IViewComponentResult Invoke(EmployeeTableViewModel viewModel)
+        public async Task<IViewComponentResult> InvokeAsync(EmployeeTableViewModel viewModel)
         {
             return View(viewModel);
         }
